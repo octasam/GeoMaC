eb7aac4e6eb4fe78b2b3af8bab1b8900a0eada5e
luiggix <luiggix@gmail.com>
2019-02-18 16:29:47 -0600

    rename

--- DF_NO_Estacionarios/1D_Heat_05.py
+++ DF_NO_Estacionarios/1D_Heat_05.py
@@ -17,65 +17,68 @@ tolerancia = 1e-6
 # Datos del dominio y la malla espacial
 a = 0.0 
 b = 1.0
-N = 50
+N = 10
 h = (b-a)/(N+1)
 
 # Datos del tiempo de simulacion y paso de tiempo
-ht = 0.0001
-Tmax = 1.0
+ht = 0.001
+Tmax = 2.0
 Nt = int(Tmax / ht)
 
-# Dato fisico (P. ej. difusividad termica)
-k = 1
+print('Nt =', Nt)
 
-print(" h = ", h)
-print(" dx^2/2k = ", h*h/(2.0*k))
-print(" ht = ", ht, " Nt = ", Nt)
+# Definicion de los arreglos donde se guardara la solucion
+x = np.linspace(a,b,N+2)
+u = np.zeros(N+2)
+
+# Dato fisico (P. ej. difusividad termica)
+#Ejemplo 1.
+k = np.ones(N+2)
+
+#Ejemplo 2.
+k[0:int(N/2)] = 0.02
+
+#Ejemplo 3.
+#k[0:int(N/4)] = 1.0
+#k[int(N/4):int(N/2)] = 0.5
+#k[int(N/2):int(3*N/2)] = 1.5
+#k[int(3*N/4):] = 2.0
+
+#Ejemplo 4.
+#k = np.random.rand(N+2)
 
 # Condiciones de frontera
 boundA = -1 # Dirichlet en a
 boundB = 1  # Direchlet en b
-
-# Definicion de los arreglos donde se guardar la solucion
-x = np.linspace(a,b,N+2)
-u = np.zeros(N+2)
 u[0] = boundA
 u[N+1] = boundB
 
-#plt.plot(x,u,'-b')
-
 sumat = 0.0
-
-r = ht * k / (h*h) 
+r = ht / (h*h) 
 
 fig = plt.figure(figsize=(8,4))           # Figuras
 ax = plt.axes(xlim=(0, 1), ylim=(-1.2, 1.2)) # Ejes
-line, = ax.plot(x, u, '--', label='FDM')
+line, = ax.plot(x, u, 'o-b', label='FDM')
 label = ax.text(0.75, -.75, 'Time = {:>8.5f}'.format(0),
                 ha='center', va='center',
                 fontsize=12)
 
-ax.plot(x,u,'b-',label='Sol. Exac',lw=2)
-plt.xlabel('$x$ [m]')
-plt.ylabel('$\phi$ [...]')
+ax.plot(x,u,'--k',label='Inicial',lw=2)
+plt.xlabel('$x$')
+plt.ylabel('$T$')
+plt.grid()
 ax.legend()
 
-title_graf = '$\partial \phi / \partial t + \partial(p u \phi)/\partial x= \partial (\Gamma \partial\phi/\partial x)/\partial x$ con FVM'
+title_graf = '$\partial T / \partial t = \partial (\Gamma \partial T/\partial x)/\partial x$'
 
 def explicitEuler(i):
     time_step = i * ht
     err = 0.0  
     for i in range(1,N+1): # Ciclo espacial
-        u_next = u[i] + r * (u[i+1] - 2 * u[i] + u[i-1])
-        err += (u_next - u[i])**2 
-        u[i] = u_next
+        unew = u[i] + r * (k[i] * u[i-1] - (k[i+1]+k[i]) * u[i] + k[i+1] * u[i+1])
+        err += (unew - u[i])**2 
+        u[i] = unew
     err = np.sqrt(h*err)
-#    t1 = time.clock()
-#    sumat += (t1 - t0)
-#    print("n = ", n, ' Error = %12.10g' % err)
-#    if ((i % 100)==0):
-#        print('time step = {:.5f} \t error = {.5f}'.format(time_step,err))
-
 #
 # Actualizamos la gráfica
 #
